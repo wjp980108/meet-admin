571468d192b3de2e15607c5fdc33d533d2154c9f
wjp <wjp_github@163.com>
2024-10-15 15:37:16 +0800

    feat(config): 新增版本检测功能

---
+++ src/config/versionRocket.ts
@@ -0,0 +1,21 @@
+import { checkVersion } from 'version-rocket';
+import { version } from '../../package.json';
+
+export default function () {
+  const { VITE_APP_NAME, VITE_PUBLIC_PATH, MODE } = import.meta.env;
+
+  checkVersion({
+    // 5分钟检测一次版本
+    pollingTime: 10000,
+    // 版本号
+    localPackageVersion: version,
+    // 版本文件地址
+    originVersionFileUrl: `${location.origin}${VITE_PUBLIC_PATH}version.json`,
+    enable: MODE === 'prod',
+  }, {
+    title: VITE_APP_NAME,
+    description: '检测到新版本',
+    buttonText: '立即更新',
+    primaryColor: '#758AFD',
+  });
+}
