18478a03490eccc9752f48e9c43f76fdee312242
wjp <wjp_github@163.com>
2024-10-15 15:58:43 +0800

    fix(other): 修改环境变量，删除冗余变量

--- src/typings/env.d.ts
+++ src/typings/env.d.ts
@@ -2,8 +2,8 @@
 interface ImportMetaEnv {
   readonly MODE: string
   readonly BASE_URL: string
-  readonly VITE_APP_ENV: string
   readonly VITE_BASE_URL: string
+  readonly VITE_PUBLIC_PATH: string
   readonly VITE_APP_PREFIX: string
   readonly VITE_APP_NAME: string
   readonly VITE_HOME_PATH: string
