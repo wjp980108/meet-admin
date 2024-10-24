df42faae34087561d39f67fa0872c8b3e84f41de
wjp <wjp_github@163.com>
2024-10-24 16:27:22 +0800

    feat(components): appText 新增功能
    - 新增 hideAfter 属性
    
    - 新增 class 属性
    
    BREAKING CHANGE: - 修改获取文本内容的方式
    
    - 去除 color 默认样式

--- src/components/AppText/index.vue
+++ src/components/AppText/index.vue
@@ -7,11 +7,14 @@ defineOptions({ name: 'AppText' });
 const props = withDefaults(defineProps<Props>(), {
   placement: 'bottom',
   offset: 5,
+  hideAfter: 0,
 });
 
 interface Props extends Partial<TextProps> {
+  class?: string
   placement?: Placement
   offset?: number
+  hideAfter?: number
 }
 
 // 是否需要显示 tooltip
@@ -45,16 +48,26 @@ function extractSlotText() {
     // 获取默认插槽的虚拟节点数组
     const vNodes = slots.default();
     // 遍历虚拟节点，提取文本节点内容
-    vNodes.forEach((node) => {
-      slotText.value = node.children as string;
-    });
+    traverseVNode(vNodes);
   }
+}
+
+// 递归遍历虚拟节点
+function traverseVNode(vNodes: VNode[]) {
+  vNodes.forEach((vNode) => {
+    if (typeof vNode.children === 'string') {
+      slotText.value = vNode.children;
+    }
+    else if (Array.isArray(vNode.children)) {
+      traverseVNode(vNode.children as VNode[]);
+    }
+  });
 }
 </script>
 
 <template>
   <el-tooltip :content="slotText" :disabled="disabled" :placement :offset>
-    <el-text v-bind="$props" @mouseover.self="handleHover">
+    <el-text class="text-unset" v-bind="$props" @mouseover.self="handleHover">
       <slot />
     </el-text>
   </el-tooltip>
